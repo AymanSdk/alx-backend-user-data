93d1077f84b557818a2c2a25d22efbd00038ba81
Aymansdk <ayman.sdk123@outlook.com>
2024-05-16 17:30:25 +0100

    feat: Add module for filtering and logging messages
    This commit adds a new module, `filtered_logger.py`, which provides functionality for filtering and logging messages. It includes a `filter_datum` function that takes a list of fields, a redaction string, a message, and a separator, and returns the filtered message by replacing the specified fields with the redaction string.
    
    The purpose of this change is to implement a mechanism for filtering sensitive information from log messages, such as personal data fields like name, email, phone, ssn, and password.

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A Module for filtering and logging messages.
+"""
+import os
+import re
+import logging
+import mysql.connector
+from typing import List
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
