496421628c0c65367d1dc05c8fc72f9852a10b1d
Aymansdk <ayman.sdk123@outlook.com>
2024-05-27 05:34:21 +0100

    feat: Update DB module for user authentication service
    This commit updates the DB module in the user_authentication_service to include necessary imports and session handling. It also initializes the database engine and drops all existing tables. These changes improve the functionality and maintainability of the user authentication service.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""DB module
+"""
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+
+from user import User, Base
+
+
+class DB:
+    """DB class
+    """
+    
+    def __init__(self):
+        """Constructor
+        """
+        self.__engine = create_engine('sqlite:///a.db', echo=False)
+        Base.metadata.drop_all(self.__engine)
