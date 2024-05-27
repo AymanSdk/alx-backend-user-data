e1a4768f32e1e2ff36a82cc11633383edee80fa7
Aymansdk <ayman.sdk123@outlook.com>
2024-05-27 05:23:40 +0100

    feat: Remove unnecessary password field in User class

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""user module
+"""
+from sqlalchemy import column, string, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """User class
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
